6abdebc602d87edffe0b4e51d21f5e3b4cb910ab
Mohamed M. Hassan <152182601+MohamedMahmoudH@users.noreply.github.com>
2024-06-24 08:14:41 +0300

    Update div3_D_GCD-sequence.cpp

--- div3_D_GCD-sequence.cpp
+++ div3_D_GCD-sequence.cpp
@@ -1,75 +1,69 @@
 #include <vector>
 #include <iostream>
-#define f(i,a,b) for(int i = a ; i < b ; ++i)
+#define loop(i,a,b) for(int i = a ; i < b ; ++i)
 #define all(x) (x).begin() , (x).end()
 using namespace std;
-
+ 
 int gcd(int a , int b)
 {
     if(!b)return a;
     return gcd(b , a % b);
 }
-
+ 
+bool isGCDSeq(vector<int>& vec)
+{
+    int sz = vec.size() , last = 0 , current = 0;
+    last = gcd(vec[0] , vec[1]);
+    loop(i,1,sz-1)
+    {
+        current = gcd(vec[i] , vec[i + 1]);
+        if(current < last)return false;
+        last = current;
+    }
+    return true;
+}
+ 
 void solve()
 {
     int n , x = 0;
     cin >> n;
-
+ 
     vector<int> arr(n);
     vector<int> GCD(n-1);
-
+ 
     cin >> arr[0];
-    for(int i = 1 ; i < n ; ++i)
+    loop(i,1,n)
     {
         cin >> arr[i];
         GCD[i - 1] = gcd(arr[i - 1] , arr[i]);
     }
-
-    bool flag = false;
-    for(int i = 1 ; i < n-1 ; ++i)
+ 
+    bool flag = true;
+    loop(i,1,n-1)
     {
         if(GCD[i] < GCD[i - 1])
         {
-            if(flag)
-            {
-                cout << "NO" << '\n';
-                return;
-            }
-
-            if(i >= 1 && i <= n-3)
-            {
-                x = gcd(arr[i - 1] , arr[i + 1]);
-                if((i != 1 && x < GCD[i - 2]) || x > GCD[i + 1])
-                {
-                    x = gcd(arr[i] , arr[i + 2]);
-                    if(x < GCD[i - 1] || (i != n-3 && x > GCD[i + 2]))
-                    {
-                        if(GCD[i] >= GCD[i - 2] && i != 1)
-                        {
-                            x = gcd(arr[i] , arr[i - 2]);
-                            if(x < GCD[i - 2] || x > GCD[i + 1])
-                            {
-                                cout << "NO" << '\n';
-                                return;
-                            }
-                            else GCD[i - 1] = GCD[i - 2] = x;
-                        }
-                        else
-                        {
-                            cout << "NO" << '\n';
-                            return;
-                        }
-                    }
-                    else GCD[i] = GCD[i + 1] = x;
-                }
-                else GCD[i] = GCD[i - 1] = x;
-            }
-            flag = true;
+            x = i;
+            flag = false;
+            break;
         }
     }
-    cout << "YES" << '\n';
+    if(flag)
+    {
+        cout << "YES\n";
+        return;
+    }
+ 
+    vector<int> check1 , check2 , check3;
+    check1 = check2 = check3 = arr;
+    check1.erase(check1.begin() + x - 1);
+    check2.erase(check2.begin() + x);
+    if(x < n - 1) check3.erase(check3.begin() + x + 1);
+ 
+    if(isGCDSeq(check1) || isGCDSeq(check2) || isGCDSeq(check3)) cout << "YES\n";
+    else cout << "NO\n";
 }
-
+ 
 int main()
 {
     ///ByMohamedMahmoud///
