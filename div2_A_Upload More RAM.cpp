d601a9ed2e8c016eb9416a2bfef0d6031243a151
Mohamed M. Hassan <152182601+MohamedMahmoudH@users.noreply.github.com>
2024-07-01 06:34:09 +0300

    Add files via upload

---
+++ div2_A_Upload More RAM.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void solve()
+{
+    short n , k;
+    cin >> n >> k;
+    cout << (n-1)*k + 1 << '\n';
+}
+
+short t;
+int main()
+{
+    ///ByMohamedMahmoud///
+    cin >> t;
+    while(t--)solve();
+    return 0;
+}
